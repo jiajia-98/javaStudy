d74953448dea1d76bc71c1346fbdba1da08f9510
unknown <980436918@qq.com>
2020-05-28 11:34:50 +0800

    5.28

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+//这是一行注释信息
+/*
+	这就是多行注释
+	123123
+	11111
+*/
+//public class 后面定义一个类的名称，类是java当中所有源代码的基本组织单位
+
+public class HelloWorld { //单行注释
+	// 第二行的内容是万年不变的固定写法 ，代表main方法
+	//这一行代表程序执行的起点
+	public static void main(String[] args){
+		//第三行代表答应输出语句（其实就是屏幕显示）
+		//希望显示什么东西，就在小括号中填写什么内容
+		System.out.println("hello world!!!");
+	}
+}
